9cec672075daad9f928c9786dc3fbc5d53e9c078
Robert Facella <rob.facella@gmail.com>
2018-03-29 00:09:53 +0000

    Script for a dependency.

---
+++ getImageMagick.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Getting ImageMagick..."
+apt-get update
+apt-get install imagemagick -y
---
+++ getJpegoptim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Getting jpegoptim..."
+apt-get update
+apt-get install jpegoptim -y
